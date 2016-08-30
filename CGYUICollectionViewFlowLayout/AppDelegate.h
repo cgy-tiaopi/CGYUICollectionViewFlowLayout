1cb9afd0f052e645c0d06df6323d0162a9c2f6b2
chengangyu <504910083@qq.com>
2016-08-30 22:01:16 +0800

    Initial Commit

---
+++ CGYUICollectionViewFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGYUICollectionViewFlowLayout
+//
+//  Created by 陈刚宇 on 16/8/30.
+//  Copyright © 2016年 CGY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CGYUICollectionViewFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CGYUICollectionViewFlowLayout
+//
+//  Created by 陈刚宇 on 16/8/30.
+//  Copyright © 2016年 CGY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
